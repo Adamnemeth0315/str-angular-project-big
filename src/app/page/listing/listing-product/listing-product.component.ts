75dd61f0832496825de7bd4f1d9095917eef7a77
szabodev <szabodev@yahoo.com>
2021-03-04 16:22:50 +0100

    part

--- src/app/page/listing/listing-product/listing-product.component.ts
+++ src/app/page/listing/listing-product/listing-product.component.ts
@@ -1,73 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
-import { Product } from 'src/app/model/product';
-import { ConfigService, ITableCol } from 'src/app/service/config.service';
-import { ProductService } from 'src/app/service/product.service';
 import { ToastrService } from 'ngx-toastr';
+import { BehaviorSubject } from 'rxjs';
+import { Customer } from 'src/app/model/customer';
+import { CustomerService } from 'src/app/service/customer.service';
+import { ConfigService, ITableCol } from 'src/app/service/config.service';
 import { StatisticsService } from 'src/app/service/statistics.service';
-
 @Component({
-  selector: 'app-listing-product',
-  templateUrl: './listing-product.component.html',
-  styleUrls: ['./listing-product.component.scss']
+  selector: 'app-listing-customer',
+  templateUrl: './listing-customer.component.html',
+  styleUrls: ['./listing-customer.component.scss'],
 })
-export class ListingProductComponent implements OnInit {
+export class ListingCustomerComponent implements OnInit {
   constructor(
-    private productService: ProductService,
+    private customerService: CustomerService,
     private router: Router,
     private config: ConfigService,
     private toastr: ToastrService,
     private statisticsService: StatisticsService
   ) { }
-
   scroll(id: string) {
     const elmnt = document.getElementById(id);
     elmnt?.scrollIntoView(false);
   }
-
-  productList: BehaviorSubject<Product[]> = this.productService.list$;
-  cols: ITableCol[] = this.config.productTableCols;
-
-
+  customerList$: BehaviorSubject<Customer[]> = this.customerService.list$;
   phrase: string = '';
-  filterKey: string = 'name';
-  filterKeys: string[] = Object.keys(new Product());
+  cols: ITableCol[] = this.config.customerTableCols;
+  filterKey: string = 'id';
+  filterKeys: string[] = Object.keys(new Customer());
   currentSelectProperty: string = 'name';
-  orderProperties: string[] = Object.keys(new Product());
+  customerProperties: string[] = Object.keys(new Customer());
   sortedOrder = 'ASC';
   sortedColumn = 'id';
   sortedCount = 0;
   column: string = '';
   direction: boolean = false;
-  columnKey: string = ''
+  columnKey: string = '';
   firstSorting = true;
-
-  numberOfActiveProducts$ = this.statisticsService.numberOfActiveProducts$;
-  numberOfAllProducts$ = this.statisticsService.numberOfAllProducts$;
-  numberOfFeaturedProducts$ = this.statisticsService.numberOfFeaturedProducts$;
-
+  numberOfAllCustomers$ = this.statisticsService.numberOfAllCustomers$;
+  numberOfActiveCustomers$ = this.statisticsService.numberOfActiveCustomers$;
+  numberOfPassiveCustomers$ = this.statisticsService.numberOfPassiveCustomers$;
   ngOnInit(): void {
-    this.productService.getAll();
+    this.customerService.getAll();
     this.statisticsService.subscribeForData();
   }
-
-  onRemove(product: Product): void {
-
-    if (!confirm(`Biztosan törli ezt a terméket? (id: ${product.id} Termék név: ${product.name} Ár: ${product.price})`)) {
+  onRemove(customer: Customer): void {
+    if (!confirm(`Biztosan törli a vásárlót? (id: ${customer.id} Keresztnév: ${customer.firstName} Vezetéknév: ${customer.lastName} Email: ${customer.email})`)) {
       return
     }
-
-    this.productService.remove(product).subscribe(
+    this.customerService.remove(customer).subscribe(
       () => {
-        this.toastr.success('Sikeresen törölted a terméket!', 'Törlés!', { timeOut: 3000 });
-        this.productService.getAll();
-        this.router.navigate(['products']);
+        this.toastr.success('Sikeresen törölted a vásárlót!', 'Törlés!', { timeOut: 3000 });
+        this.customerService.getAll();
+        this.router.navigate(['customers']);
       },
-      error => this.toastr.error('Hiba történt a termék törlésekor!', 'Hiba!', { timeOut: 3000 })
+      error => this.toastr.error('Hiba történt a vásárló törlésekor!', 'Hiba!', { timeOut: 3000 })
     )
   }
-
   onColumnSelect(columnName: string): void {
     if (this.firstSorting) {
       this.sortedOrder = 'ASC';
@@ -77,8 +66,8 @@ export class ListingProductComponent implements OnInit {
     this.sortedColumn = columnName;
     this.direction = !this.direction;
   }
-
   onChangePhrase(event: any): void {
     this.phrase = (event.target as HTMLInputElement).value;
   }
 }
+
